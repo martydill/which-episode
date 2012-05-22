736878dd71dfb0d8c38526bdcd0e864b349e94d4
Marty Dill <marty@code-ninja.org>
2012-05-21 19:12:25 -0700

    Initial stuff

---
+++ Which Episode/ShowDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ShowDetailsViewController.h
+//  Which Episode
+//
+//  Created by Marty Dill on 12-05-21.
+//  Copyright (c) 2012 Marty Dill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShowDetailsViewController : UIViewController
+
+@end
---
+++ Which Episode/ShowDetailsViewController.m
@@ -0,0 +1,43 @@
+//
+//  ShowDetailsViewController.m
+//  Which Episode
+//
+//  Created by Marty Dill on 12-05-21.
+//  Copyright (c) 2012 Marty Dill. All rights reserved.
+//
+
+#import "ShowDetailsViewController.h"
+
+@interface ShowDetailsViewController ()
+
+@end
+
+@implementation ShowDetailsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
